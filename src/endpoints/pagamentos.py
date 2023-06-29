a793194195294117805dc168230f61384c1da702
Leandro Marques <leandromqrs@hotmail.com>
2023-06-28 23:25:24 -0300

    clean: Apaga a pasta src

--- src/endpoints/pagamentos.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from src.dominio.pix import PixData
-
-router = APIRouter()
-
-@router.post('/pix')
-async def handle_pix_payment(pixData:PixData):
-    return { 'data':pixData}
-
